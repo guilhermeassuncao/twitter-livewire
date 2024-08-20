cfe773d7dbc42c058d0e57bdc3bd0e72cb34daa6
Guilherme Assunção <cgmatosassuncao@gmail.com>
2024-08-20 15:44:06 -0300

    User authentication

--- app/Livewire/ShowTweets.php
+++ app/Livewire/ShowTweets.php
@@ -2,14 +2,36 @@
 
 namespace App\Livewire;
 
+use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowTweets extends Component
 {
-    public $message = 'Teste';
+    use WithPagination;
+    
+    public $content = 'Teste';
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
-        return view('livewire.show-tweets');
+        $tweets = Tweet::with('user')->latest()->paginate(2);
+
+        return view('livewire.show-tweets', [
+            'tweets' => $tweets
+        ]);
+    }
+
+    public function create(){
+        $this->validate();
+
+        auth()->user()->tweets()->create([
+            'content' => $this->content
+        ]);
+
+        $this->content = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,4 +1,29 @@
 <div>
-    {{$message}}
-    <input type="text" wire:model.live="message">
+    {{$content}}
+    <br>
+    <form action="" method="post" wire:submit.prevent="create">
+        <input type="text" wire:model.live="content">
+        <br>
+        @error('content') {{$message}} @enderror
+        <br>
+
+        <button type="submit">Criar Tweet</button>
+    </form>
+
+
+
+    <br>
+
+    <hr>
+
+    @foreach ($tweets as $tweet)
+        {{$tweet->user->name}} - {{$tweet->content}}
+        <br>
+    @endforeach
+
+    <hr>
+
+    <div>
+        {{$tweets->links()}}
+    </div>
 </div>
